68b64b72c7f93b12a77b971c6466ea28e05af753
Claudio Figueiredo <jcfigueiredo@gmail.com>
2013-11-24 17:05:30 -0500

    Add basic Rails4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookxie::Application.config.secret_key_base = 'a0fcbe20a0674e9e979bc185e4f4aa0ca0e54737332f76b6ecaf4a4d82d4365e918cbf0ee129298c47345d798f3e9ee5193f88292459c93e21bf6db6876c0bf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookxie::Application.config.session_store :cookie_store, key: '_bookxie_session'
